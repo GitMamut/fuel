5ee35e004e5fa28ece4fd1c3149547ec1f80bdb3
Toborowicz, Przemyslaw <przemyslaw.toborowicz@sabre.com>
2020-08-29 16:16:56 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
+import bp_1 from './bp_1.jpg';
 import './App.css';
 
 function App() {
+  const worker = createWorker({
+    logger: m => console.log(m),
+  });
+
+  const video: React.RefObject<HTMLVideoElement> = React.createRef();
+  const constraints = {
+    video: true,
+  };
+
+  const doOCR = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(bp_1);
+    setOcr(text);
+  };
+
+  const [ocr, setOcr] = useState('Recognizing...');
+
+  useEffect(() => {
+      navigator.mediaDevices.getUserMedia(constraints)
+          .then((stream) => {
+              if (video.current)
+                video.current.srcObject = stream;
+          });
+    // doOCR();
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {/*<img src={bp_1} />*/}
+        {<video id="player" controls autoPlay ref={video} />}
+        <p>{ocr}</p>
+      </div>
   );
 }
 
