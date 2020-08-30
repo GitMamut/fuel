feedff40bc61f3e1f82131740601b504236c35ff
Przemek <vatier@gazeta.pl>
2020-08-30 19:36:56 +0200

    remove react create app artifacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { createWorker } from 'tesseract.js';
-import './App.css';
 
 function App() {
   const worker = createWorker({
